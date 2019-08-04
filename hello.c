74f3fa7ce42ded3fd9870e781f970e10430be3d0
LONG <1558193230@qq.com>
2019-08-04 15:47:30 +0800

    增加sum

--- hello.c
+++ hello.c
@@ -1,1 +1,7 @@
-c
+#include <stdio.h>
+#include <sum.h>
+
+int main (void)
+{
+    printf("1+1=%d\n", fun(1,1));
+}
---
+++ sum.h
@@ -0,0 +1,6 @@
+#ifndef __SUM__
+#define __SUM__
+
+int sum(int i, int j);
+
+#endif
